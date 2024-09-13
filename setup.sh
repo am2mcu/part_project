0beac5ec9c068ea2f7ed5cf8bc95ce3fedd0dcc5
am2mcu <armynm005@gmail.com>
2024-09-13 11:58:11 +0330

    refactor: define separated functions for config_ssh

--- setup.sh
+++ setup.sh
@@ -34,28 +34,52 @@ initial_setup() {
     update_packages
 }
 
-config_ssh() {
-    local ssh_config_path=/etc/ssh/sshd_config
-    
+ssh_install_package() {
     apt -qq install openssh-server
+}
 
-    sed -i '/\bPort\b/c\Port 2324' $ssh_config_path
+ssh_change_port() {
+    sed -i '/\bPort\b/c\Port 2324' $1
+}
+
+ssh_change_login_msg() {
+    local login_msg="Hello from Emperor Penguin 3"
+    local motd_path=/etc/motd
+    
+    echo $login_msg > $motd_path
 
     sed -i \
         -e '/\bUsePAM\b/c\UsePAM no' \
         -e '/\bPrintMotd\b/c\PrintMotd yes' \
         -e '/\bPrintLastLog\b/c\PrintLastLog no' \
-        $ssh_config_path
+        $1
+}
 
+ssh_block_root_login() {
     # Include '#' to avoid undesired changes
-    sed -i '/#PermitRootLogin\b/c\PermitRootLogin no' $ssh_config_path
+    sed -i '/#PermitRootLogin\b/c\PermitRootLogin no' $1
+}
 
+config_ssh() {
+    local ssh_config_path=/etc/ssh/sshd_config
+
+    ssh_install_package
+
+    ssh_change_port $ssh_config_path
+
+    ssh_change_login_msg $ssh_config_path
+
+    ssh_block_root_login $ssh_config_path
+
+    systemctl restart ssh.service
 }
 
 
 main() {
     # Program Flow
-    initial_setup
+    # initial_setup
+    
+    config_ssh
 }
 
 
