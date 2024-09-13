92466cf99a9c2b077e2fc208d8e451b4c385d689
am2mcu <armynm005@gmail.com>
2024-09-13 10:28:06 +0330

    feat: add set_mirror function

--- setup.sh
+++ setup.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-username="part"
-
 add_user() {
+    local username="part"
     useradd $username -m -G sudo
 }
 
 set_mirror() {
+    local official_repos=$(cat <<-END
+deb http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian bookworm main contrib non-free non-free-firmware
     
+deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian bookworm-updates main contrib non-free non-free-firmware
+END
+    )
+
+    local sources_list_path=/etc/apt/sources.list
+
+    echo "$official_repos" > $sources_list_path
 }
 
 
 main() {
     # Program Flow
-    initial_setup
+    # initial_setup
+    set_mirror
 }
 
 
