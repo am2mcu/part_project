c1bc56ff10026feb3ebd4d2f530a234a93c535a8
am2mcu <armynm005@gmail.com>
2024-09-13 11:33:07 +0330

    feat: add config_ssh function

--- setup.sh
+++ setup.sh
@@ -34,6 +34,25 @@ initial_setup() {
     update_packages
 }
 
+config_ssh() {
+    local ssh_config_path=/etc/ssh/sshd_config
+    
+    apt -qq install openssh-server
+
+    sed -i '/\bPort\b/c\Port 2324' $ssh_config_path
+
+    sed -i \
+        -e '/\bUsePAM\b/c\UsePAM no' \
+        -e '/\bPrintMotd\b/c\PrintMotd yes' \
+        -e '/\bPrintLastLog\b/c\PrintLastLog no' \
+        $ssh_config_path
+
+    # Include '#' to avoid undesired changes
+    sed -i '/#PermitRootLogin\b/c\PermitRootLogin no' $ssh_config_path
+
+}
+
+
 main() {
     # Program Flow
     initial_setup
